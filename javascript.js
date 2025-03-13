d0a4c86fab8f22aa1652bc343a02f711cfd28b5f
pureH4VOC <solinski.patrick@gmail.com>
2025-03-12 23:00:54 -0400

    Feat:added getComputerChoice function to javascript
    -patch

--- javascript.js
+++ javascript.js
@@ -1,1 +1,9 @@
 console.log("Hello World")
+
+// get computer source will return a random string value of either "rock" "paper" or "scissors"
+
+function getComputerChoice() {
+    const choices=["rock", "paper", "scissors"];
+    const randomIndex = Math.floor(Math.random() * choices.length)
+    return choices[randomIndex]
+}
